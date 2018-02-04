a7762eeed2e851b438f0f00e2a375ea8b625afd0
Marco Cameriero <me@marcocameriero.net>
2018-02-04 20:00:18 +0100

    Improved JS API for themes.

--- src/js/hedit.js
+++ src/js/hedit.js
@@ -3,6 +3,105 @@
 
 import EventEmitter from 'hedit/private/eventemitter';
 
+function parseHex(str) {
+    const m = str.match(/^([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})$/i);
+    if (!m) {
+        throw new Error(`Invalid color: ${str}.`);
+    }
+
+    let r = parseInt(m[1], 16);
+    let g = parseInt(m[2], 16);
+    let b = parseInt(m[3], 16);
+
+    // Grays live in another space
+    if (r == g && g == b) {
+        return 232 + Math.floor(r / 255 * 23);
+    } else {
+        r = Math.floor(r / 255 * 5);
+        g = Math.floor(g / 255 * 5);
+        b = Math.floor(b / 255 * 5);
+        return 16 + 36 * r + 6 * g + b;
+    }
+}
+
+function parseColor(c) {
+    if (typeof c === 'string') {
+        return parseHex(c);
+    } else if (typeof c === 'number') {
+        const n = Math.floor(c);
+        if (n < 0 || n > 255) {
+            throw new Error(`Invalid integer color value ${n}.`);
+        }
+        return n;
+    } else {
+        throw new Error(`Invalid color: ${c}.`);
+    }
+}
+
+function expandPen(pen, defaults) {
+    
+    // Missing property
+    if (typeof pen === 'undefined' || pen === null) {
+        return defaults;
+
+    // Hex or integer color
+    } else if (typeof pen === 'string' || typeof pen === 'number') {
+        return Object.assign({}, defaults, { fg: parseColor(pen) });
+
+    // Complex object
+    } else if (typeof pen === 'object') {
+        let clone = Object.assign({}, defaults, pen);
+        clone.fg = parseColor(clone.fg);
+        clone.bg = parseColor(clone.bg);
+        clone.bold = !!clone.bold;
+        clone.under = !!clone.under;
+        return clone;
+
+    // Invalid
+    } else {
+        throw new Error(`Invalid pen descriptor: ${pen}`);
+    }
+
+}
+
+/**
+ * The user can represent themes with a wide variety of shortcuts,
+ * but the final form of the descriptor that we have to pass to the native method is as follows:
+ *
+ * {
+ *     text: { fg: [0 - 255], bg: [0 - 255], bold: true|false, under: true|false },
+ *     linenos: ...,
+ *     error: ...,
+ *     ...
+ * }
+ *
+ * All the properties are required, and must have the same exact format.
+ *
+ * The user can also specify colors in hex, which means that we have to convert them
+ * to terminal colors.
+ */
+function expandTheme(t) {
+
+    // Default theme
+    const defaultTheme = {
+        text:         { fg: 7,   bg: 16,  bool: false, under: false },
+        linenos:      { fg: 8,   bg: 16,  bold: false, under: false },
+        error:        { fg: 1,   bg: 16,  bold: true,  under: false },
+        block_cursor: { fg: 16,  bg: 7,   bool: false, under: false },
+        soft_cursor:  { fg: 7,   bg: 16,  bold: true,  under: true  },
+        statusbar:    { fg: 234, bg: 247, bold: false, under: false },
+        commandbar:   { fg: 7,   bg: 16,  bool: false, under: false }
+    };
+
+    let penDescriptor = {};
+    const textprops = [ 'text', 'linenos', 'error', 'block_cursor', 'soft_cursor', 'commandbar', 'statusbar' ];
+    for (let k of textprops) {
+        penDescriptor[k] = expandPen(t[k], defaultTheme[k]);
+    }
+
+    return penDescriptor;
+}
+
 let hedit = new EventEmitter();
 Object.assign(hedit, {
     get mode() {
@@ -12,7 +111,7 @@ Object.assign(hedit, {
         return __hedit.view();
     },
     setTheme(t) {
-        __hedit.setTheme(t);
+        __hedit.setTheme(expandTheme(t));
     },
     emitKeys(keys) {
         __hedit.emitKeys(keys);
