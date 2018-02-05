79b210c3001abd4c18d171d2f019b966b90117f8
Marco Cameriero <me@marcocameriero.net>
2018-02-05 15:19:38 +0100

    Include a copy of the license text in the files taken from other projects.

--- src/util/map.c
+++ src/util/map.c
@@ -1,9 +1,26 @@
-/* Crit-bit tree based map which supports lookups based on unique
- * prefixes as well as ordered iteration.
- *
+/**
  * Taken from the `vis` project:
  * https://github.com/martanne/vis/blob/master/map.c
  * 
+ * Copyright © 2014-2017 Marc André Tanner, et al.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ */
+
+/* Crit-bit tree based map which supports lookups based on unique
+ * prefixes as well as ordered iteration.
+ *
  * Based on public domain code from Rusty Russel, Adam Langley
  * and D. J. Bernstein.
  *
@@ -12,6 +29,7 @@
  *  http://github.com/agl/critbit
  *  http://ccodearchive.net/info/strmap.html
  */
+
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
--- src/util/map.h
+++ src/util/map.h
@@ -1,8 +1,25 @@
+/**
+ * Taken from the `vis` project:
+ * https://github.com/martanne/vis/blob/master/map.h
+ * 
+ * Copyright © 2014-2017 Marc André Tanner, et al.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ */
+
 #ifndef __MAP_H__
 #define __MAP_H__
-
-// Taken from the `vis` project:
-// https://github.com/martanne/vis/blob/master/map.h
 
 #include <stdbool.h>
 
