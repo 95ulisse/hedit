6be31fb13f622eb01bb78fe099bd2752cf4c5b0e
Marco Cameriero <me@marcocameriero.net>
2018-03-08 14:04:52 +0100

    Basic test infrastructure.

---
+++ test/main.c
@@ -0,0 +1,10 @@
+// CTest options
+#define CTEST_MAIN
+#define CTEST_SEGFAULT
+#define CTEST_COLOR_OK
+
+#include "ctest.h"
+
+int main(int argc, const char* argv[]) {
+    return ctest_main(argc, argv);
+}
